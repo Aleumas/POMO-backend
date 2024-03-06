6deab59c5b2ff19f032a42d9e69d225c075aec0f
Samuel Asamoah <aleumas25@gmail.com>
2024-03-05 21:31:11 -0500

    first commit

---
+++ server.cjs
@@ -0,0 +1,13 @@
+const express = require("express");
+const { createServer } = require("http");
+const { Server } = require("socket.io");
+
+const app = express();
+const httpServer = createServer(app);
+const io = new Server(httpServer);
+
+io.on("connection", (socket) => {
+  console.log("connected");
+});
+
+httpServer.listen(3000);
