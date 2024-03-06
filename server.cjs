5304762aa3317bb7724be75f6d0cc5b0c10dc41d
Samuel Asamoah <aleumas25@gmail.com>
2024-03-05 23:34:57 -0500

    add timer logic

--- server.cjs
+++ server.cjs
@@ -4,10 +4,23 @@ const { Server } = require("socket.io");
 
 const app = express();
 const httpServer = createServer(app);
-const io = new Server(httpServer);
+const io = new Server(httpServer, {
+  cors: {
+    origin: "*",
+  },
+});
 
 io.on("connection", (socket) => {
-  console.log("connected");
+  socket.on("start", (time) => {
+    time + 1;
+    const timer = setInterval(() => {
+      time -= 1;
+      socket.emit("update-timer", time);
+      if (time === 0) {
+        clearInterval(timer);
+      }
+    }, 1000);
+  });
 });
 
 httpServer.listen(3000);
